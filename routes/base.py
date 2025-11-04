95d4befcf88094451fa53b7131fee54c20905d78
Mohamed Yousry <yousrymohamed999@gmail.com>
2025-11-04 15:38:20 +0200

    addinn base/__init__ files

---
+++ routes/base.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api/v1"]
+)
+
+@base_router.get("/")
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+
+    return {
+        "message": "Mini RAG App is running",
+        "app_name": app_name,
+        "app_version": app_version
+    }
